074a2860b349841b686657d20badff4647ab069a
Nis2706 <nishapatel2799@gmail.com>
2024-06-22 19:38:41 +0530

    Add files via upload

---
+++ Capstone/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const learnMoreButton = document.getElementById('learnMore');
+    learnMoreButton.addEventListener('click', () => {
+        window.location.href = '#about';
+    });
+});
